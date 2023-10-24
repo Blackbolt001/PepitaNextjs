f109b15e4cdeab8b1b0e25cc9bc101e4f9fb2b44
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-10-23 20:38:28 -0400

    color

--- components/Layout.js
+++ components/Layout.js
@@ -8,13 +8,11 @@ export default function Layout({children}) {
  if (!session) {
   return (
 
-    <div className=" bg-bkg text-content isolate overflow-hidden p-4 sm:p-8 grid place-items-center">
-    <div className="grid relative bg-bkg min-h-screen place-items-center place-content-center gap-16 before:absolute before:h-1/2 before:w-3/4 before:rounded-tr-full before:rounded-bl-full before:blur-3xl before:bg-accent-2 before:animate-spin-slower before:-z-10 after:absolute after:h-2/3 after:w-2/3 after:rounded-tr-full after:rounded-bl-full after:blur-3xl after:bg-accent-1/80 after:animate-spin-slow after:-z-10">
-    <button onClick={() => signIn('google')} className="bg-blue-200 p-2 px-4 rounded-lg">Login with Google</button>
-    <button onClick={() => signIn('facebook')} className="bg-blue-200 p-2 px-4 rounded-lg">Login with Facebook</button>
+    <div className=" bg-gradient-to-r from-yellow-200 via-amber-500 to-green-400 text-content isolate overflow-hidden p-4 sm:p-8 grid place-items-center">
+    <div className="grid relative min-h-screen place-items-center place-content-center gap-16 before:absolute before:h-1/2 before:w-3/4 before:rounded-tr-full before:rounded-bl-full before:blur-3xl before:bg-accent-2 before:animate-spin-slower before:-z-10 after:absolute after:h-2/3 after:w-2/3 after:rounded-tr-full after:rounded-bl-full after:blur-3xl after:bg-accent-1/80 after:animate-spin-slow after:-z-10">
+    <button onClick={() => signIn('google')} className="bg-yellow-800 text-yellow-200 p-2 px-4 rounded-lg">Login with Google</button>
+    <button onClick={() => signIn('facebook')} className="bg-yellow-800 text-yellow-200 p-2 px-4 rounded-lg">Login with Facebook</button>
     <div className="text-6x1 font-bold">
-    <span className="underline decoration-accent-2">Dark</span> or {""}
-    <span className="underline decoration-accent-1">Light</span>?
 </div>
 </div>
 </div>
